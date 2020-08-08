f4d1eab829c39fcf9bc9059d61d9fa6ae650a59a
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2020-08-08 08:58:48 -0300

    Finishing 'Tests and TDD' at level 4

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -1,8 +1,10 @@
 import { container } from 'tsyringe';
+import '@modules/users/providers';
 import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
 import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+import './providers';
 
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentsRepository',
