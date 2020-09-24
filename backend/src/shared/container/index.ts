f618e2249004025a27b12b022b309ca17f7e7250
Natália Oliveira <nataliaferreiraoliveira1994@gmail.com>
2020-09-23 23:40:10 -0300

    Refactoring tests

--- backend/src/shared/container/index.ts
+++ backend/src/shared/container/index.ts
@@ -6,6 +6,8 @@ import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 import IUserTokensRepository from '@modules/users/repositories/IUserTokensRepository';
 import UserTokensRepository from '@modules/users/infra/typeorm/repositories/UserTokenRepository';
+import INotificationssRepository from '@modules/notifications/repositories/INotificationsRepository';
+import NotificationsRepository from '@modules/notifications/infra/typeorm/repositories/NotificationsRepository';
 import './providers';
 
 container.registerSingleton<IAppointmentsRepository>(
@@ -21,4 +23,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IUserTokensRepository>(
   'UserTokensRepository',
   UserTokensRepository,
+);
+
+container.registerSingleton<INotificationssRepository>(
+  'NotificationsRepository',
+  NotificationsRepository,
 );
